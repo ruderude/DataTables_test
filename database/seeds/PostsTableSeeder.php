7450b6ada2e9cb61ef4074c475638f6d50d56976
Kunshi <rude1979@gmail.com>
2020-02-27 18:32:51 +0900

    プレビュー、バリデーションなど実装

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -15,10 +15,10 @@ class PostsTableSeeder extends Seeder
         $faker = Faker\Factory::create('ja_JP');
 
         // ランダムに記事を作成
-        for ($i = 0; $i < 10; $i++)
+        for ($i = 0; $i < 50; $i++)
         {
             DB::table('posts')->insert([
-                'user_id' => $faker->numberBetween(1, 3),
+                'user_id' => $faker->numberBetween(1, 4),
                 'title' => $faker->text(20),
                 'body' => $faker->text(200),
                 'created_at' => $faker->dateTime(),
